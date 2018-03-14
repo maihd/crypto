da5442ce0eeb42e5732323b31f0328e3f95a5e48
Mini HD <maihoangdung.tierger@gmail.com>
2018-03-14 14:43:19 +0700

    CRC calculation algorithms

--- test.c
+++ test.c
@@ -1,8 +1,11 @@
+#define CRC_IMPL
 #define BASE64_IMPL
+#define HAVE_CRC_PRINT_TABLE 1
 
 #include <stdio.h>
 #include "base64.h"
 #include <string.h>
+#include "crc.h"
 
 int main(int argc, char* argv[])
 {
@@ -29,6 +32,10 @@ int main(int argc, char* argv[])
     printf("n: %d\n", n);
     printf("Src: %s\n", dst);
     printf("Dst: %s\n", src);
-    
+    printf("Src CRC: %u\n", crc_calc(src, strlen(src)));
+    printf("Dst CRC: %u\n", crc_calc(dst, strlen(dst)));
+
+    //crc_init();
+    //crc_print_table();
     return 0;
 }
